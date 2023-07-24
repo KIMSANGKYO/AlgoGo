979bf691bd2b090416b9bec3f9addd030f862c88
KIMSANGKYO <sangyoes@naver.com>
2023-07-24 20:57:58 +0900

    ✨ func : 헤더 메인페이지 라우팅 추가

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -1,9 +1,11 @@
+import Link from "next/link";
 import React from "react";
 
 const Header = () => {
   return (
-    <header className="flex items-center justify-center h-16 bg-blue-400">
-      헤더
+    <header className="flex items-center justify-around h-16 bg-blue-400">
+      <Link href="/">메인으로</Link>
+      <nav>네비게이션</nav>
     </header>
   );
 };
