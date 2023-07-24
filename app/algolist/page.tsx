4ab79e7e4bfae235e757dfa04c31713270429285
KIMSANGKYO <sangyoes@naver.com>
2023-07-24 20:04:21 +0900

    ✨ func : 라우팅 기능 테스트 및 알고리즘 목록 페이지 생성

---
+++ app/algolist/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AlgoList = () => {
+  return (
+    <div>
+      <h2>알고리즘 목록페이지</h2>
+    </div>
+  );
+};
+
+export default AlgoList;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between min-h-screen p-24">
+    <main className="flex flex-col items-center min-h-screen p-24">
       <h2> 여기가 메인페이지</h2>
+      <Link href="/algolist">알고리즘 목록</Link>
     </main>
   );
 }
