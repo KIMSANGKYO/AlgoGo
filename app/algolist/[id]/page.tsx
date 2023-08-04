0ff33cef3fb538bb37501a197f9d7923949fb741
KIMSANGKYO <sangyoes@naver.com>
2023-08-04 19:37:13 +0900

    ✨ func : 문제 컴포넌트 프롭스 코드 정리 및 드래그 박스 테스트 컴포넌트 생성

--- app/algolist/[id]/page.tsx
+++ app/algolist/[id]/page.tsx
@@ -1,6 +1,6 @@
-import React from "react";
 import prisma from "@/lib/prisma";
 import NormalProblem from "@/components/algolist/NormalProblem";
+import ProblemBox from "@/components/algolist/ProblemBox";
 
 type PageParams = {
   id: number;
@@ -14,18 +14,27 @@ const ProblemList = async ({ params }: { params: PageParams }) => {
   });
 
   return (
-    <main>
-      {problem ? (
-        <NormalProblem problem={problem} />
-      ) : (
-        <div>데이터안받아짐</div>
-      )}
-      <div>{Number(params.id)}</div>
-    </main>
+    <div className="flex justify-around min-h-screen p-4 bg-myWhite">
+      <section className="flex-1 p-4 overflow-auto resize-x">
+        {problem ? (
+          <NormalProblem problem={problem} />
+        ) : (
+          <div>데이터안받아짐</div>
+        )}
+      </section>
+      <div
+        className="w-4 p-1 bg-gray-300 cursor-col-resize"
+        style={{ resize: "horizontal" }}
+      ></div>
+      <section className="flex-1 p-4 overflow-auto resize-x">
+        코드 작성 컴포넌트
+        <ProblemBox />
+      </section>
+    </div>
   );
 };
 
 export default ProblemList;
 
 // 문제 푸는 페이지
-// 파람스 받아오기 테스트
+// 파람스 받아오기 테스트 => Number(params.id)
---
+++ components/algolist/ProblemBox.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import React from "react";
+
+const ProblemBox = () => {
+  return <div>ProblemBox</div>;
+};
+
+export default ProblemBox;
