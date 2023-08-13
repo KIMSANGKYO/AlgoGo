cf90e2bab599d06117d05c21a040769e767a2058
KIMSANGKYO <sangyoes@naver.com>
2023-08-13 19:55:33 +0900

    ♻️ refact : 문제 컨테이너 테일윈드 리팩

--- components/algolist/ProblemContainer.tsx
+++ components/algolist/ProblemContainer.tsx
@@ -33,43 +33,27 @@ const ProblemContainer = ({ problem }: { problem: Problem }) => {
 
   return (
     <div
-      style={{
-        display: "flex",
-        height: "500px",
-        border: "1px solid #ccc",
-        position: "relative", // 부모 컨테이너를 relative로 설정
-      }}
+      className="relative flex border border-gray-300 h-44"
       onMouseMove={handleMouseMove}
       onMouseUp={handleMouseUp}
     >
       <section
+        className="overflow-auto"
         style={{
           width: leftWidth,
-          overflow: "auto",
         }}
       >
         <NormalProblem problem={problem} />
       </section>
       <div
+        className="absolute top-0 w-1 h-full bg-gray-300"
         style={{
-          width: "4px", // 경계선의 너비
-          height: "100%",
-          background: "#ccc", // 경계선의 색상
           cursor: dragging ? "col-resize" : "default",
-          position: "absolute", // 절대 위치로 설정
-          top: 0,
           left: leftWidth,
         }}
         onMouseDown={handleMouseDown}
       />
-      <section
-        style={{
-          flex: "1",
-          overflow: "auto",
-        }}
-      >
-        오른쪽내용
-      </section>
+      <section className="flex-1 overflow-auto">오른쪽내용</section>
     </div>
   );
 };
