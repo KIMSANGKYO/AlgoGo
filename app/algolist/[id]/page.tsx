e544c6d48cc2184fa4e2555d0fd6ef8f5a12bdb8
KIMSANGKYO <sangyoes@naver.com>
2023-08-16 16:08:30 +0900

    💄 design : 문제 상세페이지 레이아웃 및 버튼 디자인

--- app/algolist/[id]/page.tsx
+++ app/algolist/[id]/page.tsx
@@ -13,12 +13,24 @@ const ProblemList = async ({ params }: { params: PageParams }) => {
   });
 
   return (
-    <div className="flex justify-around min-h-screen p-4 bg-myWhite">
+    <div className="flex flex-col min-h-screen p-4 bg-myWhite">
       {problem ? (
         <ProblemContainer problem={problem} />
       ) : (
         <div>데이터안받아짐</div>
       )}
+      <div className="flex justify-end p-4">
+        <button className="w-20 h-10 bg-myPink">초기화</button>
+        <button className="w-20 h-10 ml-4 bg-myGreen">제출</button>
+      </div>
+      <section className="flex flex-col bg-myBlue">
+        <div>코드러닝</div>
+        {/* 테케 출력 뿌리기 */}
+        <div>출력값</div>
+        <div>정답</div>
+        <div>코드 로딩</div>
+        <div>맞틀 확인 메시지</div>
+      </section>
     </div>
   );
 };
